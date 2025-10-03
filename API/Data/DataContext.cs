f2f0072a2aa73217da3316508a3b8c564c804fee
Sergey Anikanov <anikanovs@yandex.ru>
2025-10-03 04:10:21 +0300

    DB support added

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace API.Data
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,11 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContextPool<DataContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
